b17418ac8712ddab07023460a8122a902bcb9ead
cepp <P1n4@git>
2018-12-09 09:48:53 -0300

    Criado profile de teste

---
+++ src/main/java/br/com/ceppantoja/cursomc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package br.com.ceppantoja.cursomc.config;
+
+import br.com.ceppantoja.cursomc.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile(value = "test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instanciateDatabase() throws ParseException {
+        this.dbService.instatiateTestDatabase();
+        return true;
+    }
+}

3294452e5428e05bd1e0a857ab1216dddf57481e
cepp <P1n4@git>
2018-12-09 10:00:32 -0300

    Criado profile de desenvolvimento

--- src/main/java/br/com/ceppantoja/cursomc/config/DevConfig.java
+++ src/main/java/br/com/ceppantoja/cursomc/config/DevConfig.java
@@ -2,6 +2,7 @@ package br.com.ceppantoja.cursomc.config;
 
 import br.com.ceppantoja.cursomc.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,15 +10,20 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile(value = "test")
-public class TestConfig {
+@Profile(value = "dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String dbAuto;
+
     @Bean
     public boolean instantializeDatabase() throws ParseException {
-        this.dbService.instatiateTestDatabase();
+        if(this.dbAuto != null && this.dbAuto.contains("create")) {
+            this.dbService.instatiateTestDatabase();
+        }
         return true;
     }
 }

efc63d39632daf293f313a2b8b335b0219072257
cepp <P1n4@git>
2019-05-05 17:06:09 -0300

    removido upload no arquivo principal da aplicação

--- src/main/java/br/com/ceppantoja/cursomc/CursomcApplication.java
+++ src/main/java/br/com/ceppantoja/cursomc/CursomcApplication.java
@@ -1,7 +1,5 @@
 package br.com.ceppantoja.cursomc;
 
-import br.com.ceppantoja.cursomc.service.S3Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-    @Autowired
-    private S3Service s3Service;
-
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        this.s3Service.uploadFile("/home/carlos/Downloads/ralph.jpg");
     }
 }

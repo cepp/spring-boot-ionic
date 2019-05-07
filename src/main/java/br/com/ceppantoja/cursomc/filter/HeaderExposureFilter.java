f1d99d7456283d079e83ab1740897f230a1f86f8
cepp <P1n4@git>
2019-05-06 23:33:50 -0300

    expondo header location

---
+++ src/main/java/br/com/ceppantoja/cursomc/filter/HeaderExposureFilter.java
@@ -0,0 +1,29 @@
+package br.com.ceppantoja.cursomc.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class HeaderExposureFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        ((HttpServletResponse)servletResponse).addHeader("access-control-expose-headers", "location");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
